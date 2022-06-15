3b32e6eb8a38325ea81a041f4524af439fbe0ef4
mrizkirohmat <mrizkirohmatp4k@gmail.com>
2022-06-15 17:45:16 +0700

    commit 4

--- index.js
+++ index.js
@@ -8,8 +8,20 @@ const dates = [
 ];
 
 // TODO: Buatlah fungsi createDate
-const createDate = null;
-
+const createDate = (dates, data) => {
+  let list = [];
+  if (data !== undefined) {
+    for (let i = 1; i <= dates.length; i++) {
+      list.push(Date.parse(dates[i]));
+    }
+    return list[data].toString().slice(0, -3);
+  } else {
+    for (let i = 1; i < dates.length; i++) {
+      list.push(Date.parse(dates[i]).toString().slice(0, -3));
+    }
+    return list.sort().join("-");
+  }
+};
 // ! JANGAN DIMODIFIKASI
 (() => {
   // IIFE
